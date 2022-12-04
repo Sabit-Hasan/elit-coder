6e39e801df9c343606697e6a8b05c76f595c643a
Naim Morshed <morshed.naim13@gmail.com>
2022-12-04 08:45:03 +0600

    firebase init

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATJa1kiPTikEyWljmPW9FGnibwQu5pv1A",
+  authDomain: "elite-coder.firebaseapp.com",
+  projectId: "elite-coder",
+  storageBucket: "elite-coder.appspot.com",
+  messagingSenderId: "884423966603",
+  appId: "1:884423966603:web:655f32d693f27a5401398e"
+};
+
+const app = initializeApp(firebaseConfig);
