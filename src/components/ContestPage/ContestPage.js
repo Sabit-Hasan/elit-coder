d8e4e58d8c49b5b8bf960e3154ee49938b86e5a5
Naim Morshed <morshed.naim13@gmail.com>
2023-01-01 03:06:00 +0600

    firebase read init

--- src/components/ContestPage/ContestPage.js
+++ src/components/ContestPage/ContestPage.js
@@ -1,10 +1,29 @@
 import CodeSection from "./CodeSection";
+import { useState } from "react";
+import { ref, onValue } from "firebase/database";
+import { database } from "../../firebase.config";
 import './ContestPage.css';
 
 export default function ContestPage() {
+  
+  const [data, setData] = useState({});
+  const starCountRef = ref(database, 'questions/');
+
+  onValue(starCountRef, (snapshot) => {
+    const data = snapshot.val();
+    // updateStarCount(postElement, data);
+    setData()
+  });
+
   return (
     <>
-      <CodeSection />
+      <div>
+        {/* {
+          data.map((item, index) => {
+            <p>hello</p>
+          })
+        } */}
+      </div>
     </>
   )
 }
